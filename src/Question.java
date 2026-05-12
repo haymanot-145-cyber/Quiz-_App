2e9801fe0b03232a8c9feea4e50829cd7c355594
Haymanot <Hhaymanot560@gmail.com>
2026-05-13 00:45:45 +0300

    Update Question.java

--- src/Question.java
+++ src/Question.java
@@ -1,2 +1,31 @@
 public class Question {
+    private String questionText;
+    private String[] choices;
+    private int answer;
+    
+    public Question(String questionText, String[] choices, int answer) {
+        this.questionText = questionText;
+        this.choices = choices;
+        this.answer = answer;
+    }
+    
+    public void display_question() {
+        System.out.println("\n" + questionText);
+        for (int i = 0; i < choices.length; i++) {
+            System.out.println((i + 1) + ". " + choices[i]);
+        }
+    }
+    
+    public boolean isCorrect(int userAnswer) {
+        return userAnswer == answer;
+    }
+    
+    // Add this getter
+    public String[] getChoices() {
+        return choices;
+    }
+    
+    public int getAnswer() {
+        return answer;
+    }
 }
