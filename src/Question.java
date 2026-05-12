7e6c419efa2a1f4b1be968137841f876d742fba5
Filipos <FikaduTesera@gmail.com>
2026-05-12 19:00:55 +0300

    Questions class,this manages the questionss logic.

--- src/Question.java
+++ src/Question.java
@@ -1,2 +1,31 @@
 public class Question {
+    String questionText;
+    String[] choices;
+    int Answer;
+
+    // Constructor
+    public Question(String questionText, String[] choices, int Answer) {
+
+        this.questionText = questionText;
+          this.choices = choices;
+            this.Answer = Answer;
+    }
+
+    // This method will show the question.
+    public void show_question ()  {
+
+        System.out.println(questionText);
+
+          for (int i = 0; i < choices.length; i++) {
+
+               System.out.println((i + 1) + ". " + choices[i]);
+                }
+    }
+
+    // This method checks if the answer is correct.
+    public boolean isCorrect(int userAnswer) {
+
+        return userAnswer == Answer;
+    }
 }
+
