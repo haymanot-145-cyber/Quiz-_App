68bfae57bd7d10e4045dc3e3485edc03b4f279a5
Filipos <FikaduTesera@gmail.com>
2026-05-12 23:10:04 +0300

    Questions class,this manages the questionss logic.

--- src/Question.java
+++ src/Question.java
@@ -1,22 +1,22 @@
 public class Question {
     String questionText;
     String[] choices;
-    int Answer;
+    int answer;
 
     // Constructor
-    public Question(String questionText, String[] choices, int Answer) {
+    public Question(String questionText, String[] choices, int answer) {
 
         this.questionText = questionText;
           this.choices = choices;
-            this.Answer = Answer;
+            this.answer = answer;
     }
 
     // This method will show the question.
-    public void show_question ()  {
+    public void display_question ()  {
 
         System.out.println(questionText);
 
-          for (int i = 0; i < choices.length; i++) {
+        for (int i = 0; i < choices.length; i++) {
 
                System.out.println((i + 1) + ". " + choices[i]);
                 }
@@ -25,7 +25,7 @@ public class Question {
     // This method checks if the answer is correct.
     public boolean isCorrect(int userAnswer) {
 
-        return userAnswer == Answer;
+        return userAnswer == answer;
     }
 }
 
--- src/Quiz.java
+++ src/Quiz.java
@@ -1,2 +1,83 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.io.File;
+
 public class Quiz {
+
+    private ArrayList<Question> questions = new ArrayList<>();
+        private int score = 0;
+      private int questionCount = 0;
+
+      private Random random = new Random();
+               private Scanner input = new Scanner(System.in);
+
+    // This loads the questions from file .
+    public void loadfile() {
+
+        try {
+            Scanner fileScanner = new Scanner(new File("questions.txt"));
+
+            while (fileScanner.hasNextLine()) {
+
+                String questionText = fileScanner.nextLine();
+
+                String[] choices = new String[4];
+                for (int i = 0; i < 4; i++) {
+                    if (fileScanner.hasNextLine()) {
+                        choices[i] = fileScanner.nextLine();
+                    }
+                }
+
+                if (!fileScanner.hasNextLine()) break;
+                int answer = Integer.parseInt(fileScanner.nextLine());
+
+                Question q = new Question(questionText, choices, answer);
+                questions.add(q);
+            }
+
+            fileScanner.close();
+
+        } catch (Exception e) {
+            System.out.println("Error loading file: " + e.getMessage());
+        }
+    }
+
+    // This starts the quiz
+    public void startQuiz() {
+
+        loadfile();
+
+        score = 0;
+        questionCount = 0;
+
+        while (questionCount < 10 && !questions.isEmpty()) {
+
+            int index = random.nextInt(questions.size());
+            Question q = questions.get(index);
+
+            //we use this method to  display the question.
+            q.display_question();
+
+            // get user answer
+            System.out.print("Your answer: ");
+            int userAnswer = input.nextInt();
+
+            // check answer
+            if (q.isCorrect(userAnswer)) {
+                score++;
+                System.out.println("Correct!");
+            } else {
+                System.out.println("Wrong!");
+            }
+
+            // remove question (no repeat)
+            questions.remove(index);
+
+            questionCount++;
+        }
+
+        System.out.println("\nQuiz Finished!");
+        System.out.println("Score: " + score + "/10");
+    }
 }
